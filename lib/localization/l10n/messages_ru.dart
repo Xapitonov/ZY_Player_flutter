69099f9d9afbb40fa047ab4cdb205c906a6edaa7
Xapitonov <92250089+Xapitonov@users.noreply.github.com>
2025-08-08 11:07:11 +0400

    Create messages_ru.dart
    Added a Russian translation.

---
+++ lib/localization/l10n/messages_ru.dart
@@ -0,0 +1,43 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ru locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ru';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function>{
+        "forgotPasswordLink": MessageLookupByLibrary.simpleMessage("Забыли пароль"),
+        "inputPasswordHint": MessageLookupByLibrary.simpleMessage("Пожалуйста, введите пароль"),
+        "inputPhoneHint": MessageLookupByLibrary.simpleMessage("Пожалуйста, введите номер телефона"),
+        "inputPhoneInvalid": MessageLookupByLibrary.simpleMessage("Пожалуйста, введите корректный номер телефона"),
+        "inputUsernameHint": MessageLookupByLibrary.simpleMessage("Пожалуйста, введите имя пользователя"),
+        "inputVerificationCodeHint": MessageLookupByLibrary.simpleMessage("Пожалуйста, введите код подтверждения"),
+        "login": MessageLookupByLibrary.simpleMessage("Вход"),
+        "noAccountRegisterLink": MessageLookupByLibrary.simpleMessage("Нет аккаунта? Зарегистрируйтесь"),
+        "openYourAccount": MessageLookupByLibrary.simpleMessage("Откройте свой аккаунт"),
+        "passwordLogin": MessageLookupByLibrary.simpleMessage("Вход по паролю"),
+        "register": MessageLookupByLibrary.simpleMessage("Регистрация"),
+        "title": MessageLookupByLibrary.simpleMessage("ZY_Player_flutter"),
+        "verificationButton": MessageLookupByLibrary.simpleMessage("Код не отправлен, просто войдите!"),
+        "verificationCodeLogin": MessageLookupByLibrary.simpleMessage("Вход по коду подтверждения"),
+        "getVerificationCode": MessageLookupByLibrary.simpleMessage("Получить код подтверждения"),
+        "confirm": MessageLookupByLibrary.simpleMessage("Подтвердить"),
+        "resetLoginPassword": MessageLookupByLibrary.simpleMessage("Сбросить пароль для входа"),
+        "registeredTips": MessageLookupByLibrary.simpleMessage("Незарегистрированный номер телефона, пожалуйста, ")
+  };
+}
